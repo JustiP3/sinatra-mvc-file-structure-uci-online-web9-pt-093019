c08e23c7a36e9b4a203ba811442fe7ac6dbe4b11
Justin Phillips <justinphil02@yahoo.com>
2020-01-06 13:26:04 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,11 @@
 class Dog
   attr_accessor :name, :age, :breed
+  @@all = []
 
   def initialize (name, age, breed)
     @name = name
     @age = age
     @breed = breed
+    @@all.push(self)
   end
 end 
